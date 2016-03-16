63206608349d64fa33a72166151c371e0015a8c1
Edouard <edou4rd@gmail.com>
2016-03-16 17:06:31 +0100

    metadata: allow mapping for copyrights and lat_text

--- superdesk/metadata/item.py
+++ superdesk/metadata/item.py
@@ -108,17 +108,14 @@ metadata_schema = {
     # Copyright Information
     'usageterms': {
         'type': 'string',
-        'mapping': not_analyzed,
         'nullable': True,
     },
     'copyrightnotice': {
         'type': 'string',
-        'mapping': not_analyzed,
         'nullable': True
     },
     'copyrightholder': {
         'type': 'string',
-        'mapping': not_analyzed,
         'nullable': True
     },
     # Category Details
@@ -308,8 +305,7 @@ metadata_schema = {
     },
     'alt_text': {
         'type': 'string',
-        'nullable': True,
-        'mapping': not_analyzed,
+        'nullable': True
     },
 
     # aka Locator as per NewML Specification
